976aba9e8f36afcf120e8870710746b658873679
Taylover <2236373829@qq.com>
2024-04-11 18:07:03 +0800

    添加nacos注册中心

---
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalController.java
@@ -0,0 +1,5 @@
+package com.atguigu.yygh.hosp.controller;/**
+    @author xyzZero3
+    @create 2024-04-09 20:44
+*/public class HospitalController {
+}
---
+++ service_client/service_client_cmn/src/main/java/com/atguigu/yygh/cmn/client/DictFeignClient.java
@@ -0,0 +1,5 @@
+package com.atguigu.yygh.cmn.client;/**
+    @author xyzZero3
+    @create 2024-04-10 13:03
+*/public interface DictFeignClient {
+}
